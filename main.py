1b8cc3d1bd8f7a15cffb10b572c98d3b94e079d8
Bahadır <dogrubahadir@gmail.com>
2022-08-01 12:55:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import tweepy
 import time
 import sys
@@ -33,46 +28,53 @@ def get_twitter_api(keys):
 
 def getParameters():
     parameters = sys.argv
-    del parameters[0]
-    return parameters
+    if parameters:
+        del parameters[0]
+        return parameters
+    else:
+        return None
 
 def process():
 
     parameters = getParameters()
-    account = parameters[0]
-    if account == "gencdamlakitap":
-        api = get_twitter_api(gencdamlakitapkeys)
-    elif account == "damlayayinevi":
-        api = get_twitter_api(damlayayinevikeys)
-    elif account == "mihrabadyayin":
-        api = get_twitter_api(mihrabadyayinkeys)
-    elif account == "humayunyayin":
-        api = get_twitter_api(humayunyayinkeys)
-    elif account == "edamladigital":
-        api = get_twitter_api(edamladigitalkeys)
+    if parameters:
+        account = parameters[0]
+        if account == "gencdamlakitap":
+            api = get_twitter_api(gencdamlakitapkeys)
+        elif account == "damlayayinevi":
+            api = get_twitter_api(damlayayinevikeys)
+        elif account == "mihrabadyayin":
+            api = get_twitter_api(mihrabadyayinkeys)
+        elif account == "humayunyayin":
+            api = get_twitter_api(humayunyayinkeys)
+        elif account == "edamladigital":
+            api = get_twitter_api(edamladigitalkeys)
+        else:
+            print("Account not found")
+            exit()
+
+        followers = api.get_follower_ids()
+        print("Followers", len(followers))
+
+        friends = api.get_friend_ids()
+        print("You follow:", len(friends))
+
+        non_friends = [friend for friend in friends if friend not in followers]
+        print("Non-follower Friends:", len(non_friends) )
+
+        logfilename = account + "_non_friends.txt"
+        logfile = open(logfilename,"a")
+        print("Friendship destroy process is started... Please Wait to finish")
+        for nonfriend in non_friends:
+            user = api.get_user(user_id = nonfriend)
+            screen_name = user.screen_name
+            logfile.write(str(nonfriend) + "-" + screen_name + "\n")
+            api.destroy_friendship(user_id = nonfriend)
+        print(len(non_friends),"non-follower friendship destroyed")
+        logfile.close()
     else:
         print("Account not found")
         exit()
-
-    followers = api.get_follower_ids()
-    print("Followers", len(followers))
-
-    friends = api.get_friend_ids()
-    print("You follow:", len(friends))
-
-    non_friends = [friend for friend in friends if friend not in followers]
-    print("Non-follower Friends:", len(non_friends) )
-
-    logfilename = account + "_non_friends.txt"
-    logfile = open(logfilename,"a")
-    print("Friendship destroy process is started... Please Wait to finish")
-    for nonfriend in non_friends:
-        user = api.get_user(user_id = nonfriend)
-        screen_name = user.screen_name
-        logfile.write(str(nonfriend) + "-" + screen_name + "\n")
-        api.destroy_friendship(user_id = nonfriend)
-    print(len(non_friends),"non-follower friendship destroyed")
-    logfile.close()
 
 if __name__ == "__main__":
  process()
